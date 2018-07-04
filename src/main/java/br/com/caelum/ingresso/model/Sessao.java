d70bea735c202c465785fd3c569e35e6ebf5d6a3
Rafael Matsuda <rafael.matsuda@matsusoft.com.br>
2018-07-03 22:17:26 -0300

    Implementado a funcionalidade de criar sessões

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,13 +2,25 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
-public class Sessao {
+import javax.persistence.*;
 
+@Entity
+public class Sessao {
+	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	
+	@ManyToOne
 	private Filme filme;
 	
+	/**
+	 * @deprecated hibernate only
+	 */
 	public Sessao(LocalTime horario, Filme filme, Sala sala){
 		this.horario = horario;
 		this.filme = filme;
